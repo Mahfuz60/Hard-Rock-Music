ee6a97aabe2835967087bbba17af3be016dbb90e
Mahfuz <you@example.com>
2021-02-10 02:29:58 +0600

    hard rock

--- rock.js
+++ rock.js
@@ -1,14 +1,15 @@
-const searchSong=()=>{
-    const searchText=document.getElementById("search-field").value;
-    const url=`https://api.lyrics.ovh/suggest/:${searchText}`;
-    // load data
-    fetch(url)
-        .then(response=>response.json())
-        .then(dataJson=>{displaySong(dataJson.data)
+const searchSong = () => {
+  const searchText = document.getElementById("search-field").value;
+  const url = `https://api.lyrics.ovh/suggest/:${searchText}`;
+  // load data
+  fetch(url)
+    .then((response) => response.json())
+    .then((dataJson) => {
+      displaySong(dataJson.data);
     })
     // error search
-    .catch(error=>displayError('Something Wrong!Please try again letter')) 
-}
+    .catch((error) => displayError("Something Wrong!Please try again letter"));
+};
 
 // const searchSong=async()=>{
 //     const searchText=document.getElementById("search-field").value;
@@ -20,15 +21,15 @@ const searchSong=()=>{
 
 // }
 
-const displaySong=songList=>{
-    // console.log(songList);
-    songList.forEach(song=>{
-        // console.log(song);
-        const songContainer=document.getElementById('song-container');
-        songContainer.innerHTML='';
-        const songDiv=document.createElement('div');
-        songDiv.className="single-result row align-items-center my-3 p-3";
-        songDiv.innerHTML=`
+const displaySong = (songList) => {
+  // console.log(songList);
+  songList.forEach((song) => {
+    // console.log(song);
+    const songContainer = document.getElementById("song-container");
+    songContainer.innerHTML = "";
+    const songDiv = document.createElement("div");
+    songDiv.className = "single-result row align-items-center my-3 p-3";
+    songDiv.innerHTML = `
         <div class="col-md-9">
         <h3 class="lyrics-name">${song.title}</h3>
         <p class="author lead">Album by <span>${song.artist.name}</span></p>
@@ -42,10 +43,9 @@ const displaySong=songList=>{
         </div>
 
         `;
-        songContainer.appendChild(songDiv);
-
-    });
-}
+    songContainer.appendChild(songDiv);
+  });
+};
 
 // const getLyric=async( artist,title)=>{
 //     const url=`https://api.lyrics.ovh/v1/:${artist}/:${title}`;
@@ -53,28 +53,27 @@ const displaySong=songList=>{
 //         .then((response)=>response.json())
 //         .then(data=>displayLyrics(data.lyrics))
 // }
-    
-const getLyric=async( artist,title)=>{
-    const url=`https://api.lyrics.ovh/v1/:${artist}/:${title}`;
-    try{
-        const response=await fetch(url);
-    const data=await response.json();
+
+const getLyric = async (artist, title) => {
+  const url = `https://api.lyrics.ovh/v1/:${artist}/:${title}`;
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
     displayLyrics(data.lyrics);
-    }
+  } catch (error) {
     // error search
-    catch(error){
-        displayError('Something went wrong!Please Try again letter');
-    }
-}
+    displayError("Something went wrong!Please Try again letter");
+  }
+};
 
 // lyrics display function
-const displayLyrics=lyrics=>{
-    const lyricsDiv=document.getElementById('songLyrics');
-    lyricsDiv.innerText=lyrics;
-}
+const displayLyrics = (lyrics) => {
+  const lyricsDiv = document.getElementById("songLyrics");
+  lyricsDiv.innerText = lyrics;
+};
 
 // error function
-const displayError=error=>{
-    const errorTag =document.getElementById('errorMsg');
-    errorTag.innerText=error;
-}
+const displayError = (error) => {
+  const errorTag = document.getElementById("errorMsg");
+  errorTag.innerText = error;
+};
